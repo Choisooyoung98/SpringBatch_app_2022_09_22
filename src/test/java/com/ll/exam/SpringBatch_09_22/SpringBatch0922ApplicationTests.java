27bb6e8db433eadeccf6debd5b1b2bac41d9db18
Choisooyoung98 <ekfms15815@gmail.com>
2022-09-22 10:30:23 +0900

    스프링 배치 세팅

---
+++ src/test/java/com/ll/exam/SpringBatch_09_22/SpringBatch0922ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.SpringBatch_09_22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch0922ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
